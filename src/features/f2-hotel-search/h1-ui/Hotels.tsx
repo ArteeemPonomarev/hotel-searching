2f06a88ff5c098a3fc2915bd64b484a58cbbb176
ArteeemPonomarev <artponomarev1994@gmail.com>
2021-10-20 07:31:34 +0300

    add hotel search from

--- src/features/f2-hotel-search/h1-ui/Hotels.tsx
+++ src/features/f2-hotel-search/h1-ui/Hotels.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import 'antd/dist/antd.css';
 import style from './Hotels.module.css';
-import {Form} from "antd";
+import {Button, DatePicker, Form, Input} from "antd";
 import {HotelsCarousel} from "../../f3-hotelsCarousel/HotelsCarousel";
 import {useSelector} from "react-redux";
 import {AppStateType} from "../../../main/m2-bll/store";
@@ -25,8 +26,53 @@ export const Hotels = () => {
                 </header>
                 <div className={style.content_wrapper}>
                     <aside className={style.hotels_searchFrom}>
-                        <Form>
+                        <Form className={style.searchForm_location}>
+                            <Form.Item
+                                name="location"
+                                rules={[
+                                    {
+                                        type: 'email',
+                                        message: 'The input is not valid E-mail!',
+                                    },
+                                    {
+                                        required: true,
+                                        message: 'Please input city!',
+                                    },
+                                ]}
+                            >
+                                <Input type="text"
+                                       name="location"
+                                       placeholder="Введите город"
+                                />
+                            </Form.Item>
+                            <Form.Item
+                                name="checkInDate"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: 'Please input check-in date!',
+                                    },
+                                ]}>
+                                <DatePicker className={style.datepicker_checkIn}/>
+                            </Form.Item>
+                            <Form.Item
+                                name="checkOutDate"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: 'Please input amount of days!',
+                                    },
+                                ]}
+                            >
+                                <Input type="text"
+                                       placeholder="Введите количество дней"/>
+                            </Form.Item>
 
+                            <Form.Item>
+                                <Button type="primary" htmlType="submit" className={style.searchForm_btn} >
+                                    Войти
+                                </Button>
+                            </Form.Item>
                         </Form>
                     </aside>
                     <main className={style.hotels_content}>
